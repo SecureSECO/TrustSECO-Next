cb91f30d95196ec5f23faf39288b4cc94f0eb74c
Gijs Blanken <blankengijs@gmail.com>
2022-06-12 16:35:31 +0200

    Changed access token

--- run.sh
+++ run.sh
@@ -11,5 +11,6 @@ fi
 
 echo 'Starting up TrustSECO'
 
-echo "REDACTED_HISTORICAL_TOKEN" | docker login ghcr.io -u "TrustSECO-Machine-User" --password-stdin
+pat=$(echo 'Z2hwX0JSR3UxQjR1eWlKa1RLVFk4ZjI2Q0JZejZHZDduUTJQV3JDOAo=' | base64 -d)
+echo $pat | docker login ghcr.io -u "TrustSECO-Machine-User" --password-stdin
 docker-compose up
