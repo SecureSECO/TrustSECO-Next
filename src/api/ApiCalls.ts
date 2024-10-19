da60632342c808b837ade3ac79a67e905e008648
Martijn Voordouw <martijnvoordouw@gmail.com>
2024-10-19 11:54:35 +0200

    small refactor

--- src/api/ApiCalls.ts
+++ src/api/ApiCalls.ts
@@ -9,12 +9,7 @@ let platform_cached: string[] | null = null;
 export async function getAllPlatforms(): Promise<string[]|null> {
   if (platform_cached === null) {
     try {
-      // the libraries.io documentation states that a api token is required
-      // but it seems like not having a token just makes it slower
-      const token = (await spiderApi.getTokens()).libraries_token;
-      let url = "https://libraries.io/api/platforms";
-      if (token !== null) url += `?api_key=${token}`
-      const { data } = await axios.get(url);
+      const { data } = await librariesApiCall("platform");
       const platforms = data.map((platform: any) => platform.name);
       platform_cached = platforms;
       return platforms;
@@ -24,4 +19,16 @@ export async function getAllPlatforms(): Promise<string[]|null> {
     }
   }
   return platform_cached;
+}
+
+/** perform a libraries.io api call. Resource is what comes after
+ * libraries.io/api in the url*/
+export async function librariesApiCall(resource: string): Promise<AxiosResponse> {
+    // the libraries.io documentation states that a api token is required
+    // but it seems like not having a token just makes it slower
+    const token = (await spiderApi.getTokens()).libraries_token;
+    let url = `https://libraries.io/api/${resource}`;
+    if (token !== null) url += `?api_key=${token}`
+    const resp = await axios.get(url);
+    return resp
 }
