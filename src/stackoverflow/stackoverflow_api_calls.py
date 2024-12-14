5507ff7a47f8ee2f3a90dca3386f533c5062b134
Martijn Voordouw <martijnvoordouw@gmail.com>
2024-12-14 11:43:38 +0100

    switched to working stack exchange endpoint for stackoverflow stuff
    other api was no longer available

--- src/stackoverflow/stackoverflow_api_calls.py
+++ src/stackoverflow/stackoverflow_api_calls.py
@@ -5,56 +5,67 @@ This file contains the logic for crawling through the [Stack Overflow website](h
 This API calls are done by using the [Requests](https://requests.readthedocs.io/en/latest/) library.
 """
 
-# Import for improved logging
 import logging
-# Import for sending and handling HTTP requests
+from datetime import datetime, timedelta, timezone
 import requests
+from typing import Any
 
 
 class StackOverflowAPICall:
     """Class methods for getting data from Stack Overflow"""
 
-    def get_monthly_popularity(self, package: str) -> float:
+    def get_monthly_popularity(self, package: str) -> float | None:
         """Function to get the monthly popularity of a package.
 
-        The data required is retrieved from the [Stack Overflow Insights API endpoint](https://insights.stackoverflow.com/trends/get-data)
+        The data is retrieved from the stack exchange api.
 
         Args:
             package (str): The name of the package.
 
         Returns:
-            float: The latest monthly popularity of the given package. This popularity is the percentage of questions posted that were about the given package.
+            float: The popularity in the latest 30 days of the package. This popularity
+                is the percentage of questions posted that were about the given package.
         """
+        logging.info("Getting monthly popularity")
+        # Time stamp 30 days ago in unix epoch time
+        from_time = (
+            datetime.now(timezone.utc)
+            - datetime(1970, 1, 1, tzinfo=timezone.utc)
+            - timedelta(days=30)
+        ).total_seconds()
 
-        logging.info('Getting monthly popularity')
+        # request url to get all questions from last month
+        url_total = f"https://api.stackexchange.com/2.3/questions?fromdate={round(from_time)}&site=stackoverflow&filter=total"
+        # request url to get all questions for this package from last month
+        url_package = f"{url_total}&tagged={package}"
 
-        # Take the url for Stack Overflow trends
-        url = 'https://insights.stackoverflow.com/trends/get-data'
+        # If both requests were succesfull calculate percentage and return
+        if (resp_total := self.make_stackoverflow_request(url_total)) and (
+            resp_package := self.make_stackoverflow_request(url_package)
+        ):
+            total = resp_total["total"]
+            total_package = resp_package["total"]
+            return (total_package / total) * 100
+        else:
+            return None
 
-        # Extract the data as json
+    def make_stackoverflow_request(self, url: str) -> Any | None:
+        """Performs a request to the stackexchange api
+
+        Args:
+            url (str): full url to requests
+
+        Returns:
+            Any | None: The json content of the response, none if an error occured
+        """
+        # NOTE: using api keys would be better, but there is rate limit of
+        # 10000 requests per ip so no keys should be fine.
         try:
             response = requests.get(url).json()
+            return response
         except requests.exceptions.RequestException as e:
             logging.error(e)
             return None
-
-        # Make sure we got the correct data
-        if 'TagPercents' not in response:
-            logging.error('Could not find TagPercents in response')
-            return None
-
-        # Make sure we got the correct data
-        if response['TagPercents'] is None:
-            logging.error('TagPercents is None')
-            return None
-
-        # Make sure that the package is in the response
-        if package not in response['TagPercents']:
-            logging.warning('Package not found in response')
-            return 0
-
-        # Return the latest monthly popularity of the given package
-        return response['TagPercents'][package][-1]
 
 
 """
